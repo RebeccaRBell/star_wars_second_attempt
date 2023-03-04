c8bf3a5c5116ab6a50befb52845981848f83b267
Rebecca Bell <rebeccaromanobell@gmail.com>
2023-03-04 23:14:23 +0000

    styling added and films in episode order

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -6,10 +6,13 @@ const FilmList = ({films, handleSelectChange}) => {
         const ListOfFilms = films.map((film, index) => {
           return <Film film={film} index={index} key={index} handleSelectChange={handleSelectChange}/>
         })
-
+        const orderedFilms = [];
+        const orderFilms1 = ListOfFilms.slice(3,6);
+        const orderFilms2 = ListOfFilms.slice(0,3);
+        orderedFilms.push(orderFilms1, orderFilms2);
   return (
         <div className='film-list'>
-    <ul>{ListOfFilms}</ul>
+    <ul>{orderedFilms}</ul>
     </div>
   )
 }
